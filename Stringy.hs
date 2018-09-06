425f6889d7d26ebda7ff98320c07562f44ef9724
jadanis <joshallendanis@gmail.com>
2018-09-06 11:43:36 -0400

    Update Stringy.hs

--- Stringy.hs
+++ Stringy.hs
@@ -1,4 +1,4 @@
-module String
+module Stringy
 (
 intr
 ) where
@@ -10,12 +10,6 @@ import Control.Monad (join)
 -- map function over a string 
 applyTo :: (Int -> Int) -> String -> String
 applyTo f = map (chr . (`mod` 128) . f . ord)
-
--- left and right bit shifts
-(.>) :: Bits a => a -> a
-(.>) = flip shiftR 1
-(<.) :: Bits a => a -> a
-(<.) = flip shiftL 1
 
 -----------------------------------------------------
 -- The following is a list of the Stingy functions --
@@ -40,12 +34,17 @@ form3 f xs x ys = (f xs)++ys
 
 -- The identity function
 id' = form0 id
+-- The increment function
 inc = form1 succ
 -- The decrement function
 dec = form1 pred
 -- The shift left function
+(<.) :: Bits a => a -> a
+(<.) = flip shiftL 1
 shiftSL = form1 (<.)
 -- The shift right function
+(.>) :: Bits a => a -> a
+(.>) = flip shiftR 1
 shiftSR = form1 (.>)
 -- The append digit function
 appendDigit :: Int -> String -> Char -> String -> String
@@ -81,44 +80,89 @@ shuff' xs
           zipple (p,q) = foldl (\acc (x,y) -> acc++[x,y]) "" $ zip p q
 
 shuff = form3 shuff'
+-- The greater than or equal function
+grteq xs x ys = (init xs)++(if (last xs) >= (head ys) then ['1'] else ['0'])++(tail ys)
+-- The if-then statement
+ifst xs x ys
+    | odd . ord . last $ xs = (init xs)++(removeBrace ys)
+    | otherwise = (init xs)++(tail $ dropWhile (/= '}') ys)
+    where removeBrace = (\st -> (takeWhile (/= '}') st)++(tail $ dropWhile (/= '}') st))
+-- Uses up the current char but changes nothing otherwise
+use = form1 id
 -- The terminate funcion
-end = form1 id
+-- Same as use just another name for clarity
+end = use
 
+---------------------------------------------------------
+-- hand (for handle) calls the string function based   --
+-- on the current character selected. Originally was a --
+-- list of functions here using guards                 --
+-- Some of this behavior is undefined yet - 09/06/18   --
+---------------------------------------------------------
+hand :: Char -> (String -> Char -> String -> String)
+hand x
+    | x == ' ' = id' -- read string input handled elsewhere
+    | x == '!' = use -- back skip function handled elsewhere
+    | x == '\"' = id' -- undefined
+    | x == '#' = id' -- read number input handled elsewhere
+    | x == '$' = use -- skip function handled elsewhere
+    | x == '%' = mod'
+    | x == '&' = id' -- undefined
+    | x == '\'' = id' -- undefined
+    | x == '(' = front
+    | x == ')' = back
+    | x == '*' = mult
+    | x == '+' = plus
+    | x == ',' = use -- set command pointer to 0, handled elsewhere
+    | x == '-' = minus
+    | x == '.' = end
+    | x == '/' = divi
+    | isDigit x = (appendDigit $ read [x])
+    | x == ':' = id' -- undefined
+    | x == ';' = id' -- subroutine function handled elsewhere
+    | x == '<' = shiftSL
+    | x == '=' = id' -- undefined
+    | x == '>' = shiftSR
+    | x == '?' = shuff
+    | x == '@' = id' -- jump function handled elsewhere
+    | isLetter x = id' -- Letters are non-ops 
+    | x == '[' = inc
+    | x == '\\' = grteq
+    | x == ']' = dec
+    | x == '^' = piv
+    | x == '_' = rep
+    | x == '`' = id' -- undefined
+    | x == '{' = ifst
+    | x == '|' = id' -- undefined
+    | x == '}' = id' -- part of the ifst
+    | x == '~' = rev
+    | x == '\DEL' = id' -- undefined
+    | otherwise = id' -- ASCII 0 thru 31 non ops 
 
+-- Changes command pointer based on command
+determinePointer :: Char -> Int -> Int
+determinePointer x n
+    | ((isLetter x) || (x == '(') || (x == '$') || (x == '^')) = (n + 1)
+    | (x == '!') = (n - 2)
+    | (x == ',') = 0
+    | otherwise = n
 
+-- Most of the program interpretation can be handled here
+intr :: Int -> String -> String
+intr n prog
+    | prog == "" = error "No program was provided!" -- error on empty string
+    | (n >= length prog) || (n < 0) = intr (n `mod` (length prog)) prog 
+    | x == '.' = end xs x ys -- done no call to intr
+    | x == ';' = intr n ((intr 0 xs)++ys) -- call intr on xs at 0 before resuming with the whole result at n
+    | x == '@' = intr (ord (head ys)) (xs++(tail ys)) -- set command point to the ord of head of ys
+    | otherwise = intr m newprog -- all other commands handled here
+    where x = (prog!!n) -- char at current command pointer
+          xs = fst $ splitAt n prog -- string before at current command
+          ys = tail . snd $ splitAt n prog -- string after current command
+          newprog = (hand x) xs x ys -- string result after current command
+          m = if x == '_' then (length newprog) - (length ys) else determinePointer x n -- special case for _ otherwise use determinePointer
 
-
-grteq xs x ys = (init xs)++((if (ord (last xs)) >= (ord (head ys)) then (chr 1) else (chr 0)):[])++(tail ys)
-
-
-
-comlist = [id',id',id',id',id',mod',id',id',front,back,mult,plus,id',minus,end,divi,(appendDigit 0),(appendDigit 1),(appendDigit 2),(appendDigit 3),(appendDigit 4),(appendDigit 5),(appendDigit 6),(appendDigit 7),(appendDigit 8),(appendDigit 9),id',id',shiftSL,id',shiftSR,shuff,id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',inc,grteq,dec,piv,rep,id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',id',rev,id']
-
-com x = comlist!!((ord x) - 32)
-hand xs x ys = (com x) xs x ys
-lett x = ((n > 64) && (n < 91)) || ((n > 96) && (n < 123)) where n = ord x
-split_ x n = map (x!!) [0..(n-1)]
-_split x n = map (x!!) [(n+1)..((length x)-1)]
-
-intr n x
-    |length x == 0 = "" 
-    |(n >= length x) || (n < 0) = intr (n `mod` (length x)) x 
-    |(x!!n)=='.' = xs++ys 
-    |(x!!n)==';' = intr n ((intr 0 xs)++ys)
-    |(x!!n)=='@' = intr (ord (head ys)) (xs++(tail ys))
-    |(x!!n)=='#' = intr 0 (xs++ys)
-    |(x!!n)=='$' = intr (n+1) (xs++ys)
-    |(x!!n)=='!' = intr (n-1) (xs++ys)
-    |(x!!n)=='('  = intr (n+1) z
-    |(x!!n)=='^' = intr (n+1) z
-    |(x!!n)=='_' = intr m z
-    |lett (x!!n) = intr (n+1) x 
-    |otherwise = intr n z
-    where xs = split_ x n
-          ys = _split x n
-          z = hand xs (x!!n) ys
-          m = (length z) - (length ys)
-
+-- handling IO functions - In progress
 intToSty' n
     |n < 128 = str
     |otherwise = (intToSty' (quot n 128))++str
@@ -128,17 +172,3 @@ intToSty = do
     line <- getLine
     let numb = intToSty' . read $ line
     return numb
-
-removeItem _ [] = []
-removeItem x (y:ys) | x==y = ys
-    | otherwise = y:(removeItem x ys)
-
-removeBefore' x (y:ys) | x==y = ys
-                       | otherwise = removeBefore' x ys
-removeBefore x (y:ys) = if (elem x (y:ys)) then (removeBefore' x (y:ys)) else ys
-
-ifst xs x ys
-    |odd . ord . last $ xs = xs++(removeItem '}' ys)
-    |otherwise = xs++zs
-    where zs = removeBefore '}' ys
-
